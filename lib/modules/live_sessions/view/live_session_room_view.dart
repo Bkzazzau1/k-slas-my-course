88459ab90bdec7a0970ae9fd698ee923738a40d9
Bkzazzau1 <ibrahim.yahaya@miva.edu.ng>
2026-06-12 10:34:03 +0100

    Use professional live classroom shell

--- lib/modules/live_sessions/view/live_session_room_view.dart
+++ lib/modules/live_sessions/view/live_session_room_view.dart
@@ -1,12 +1,12 @@
 import 'package:flutter/material.dart';
 
-import 'live_class_room_shell.dart';
+import 'professional_live_class_room_shell.dart';
 
 class LiveSessionRoomView extends StatelessWidget {
   const LiveSessionRoomView({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return const LiveClassRoomShell();
+    return const ProfessionalLiveClassRoomShell();
   }
 }
