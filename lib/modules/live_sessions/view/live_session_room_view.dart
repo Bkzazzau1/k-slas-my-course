072854361f5e4212909f885d869eb5c20631f7aa
Bkzazzau1 <ibrahim.yahaya@miva.edu.ng>
2026-06-12 10:36:40 +0100

    Route live session room to responsive shell

--- lib/modules/live_sessions/view/live_session_room_view.dart
+++ lib/modules/live_sessions/view/live_session_room_view.dart
@@ -1,12 +1,12 @@
 import 'package:flutter/material.dart';
 
-import 'professional_live_class_room_shell.dart';
+import 'professional_live_class_room_shell_v2.dart';
 
 class LiveSessionRoomView extends StatelessWidget {
   const LiveSessionRoomView({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return const ProfessionalLiveClassRoomShell();
+    return const ProfessionalLiveClassRoomShellV2();
   }
 }
