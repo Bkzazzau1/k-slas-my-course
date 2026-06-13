1adfcd646a419c4c798b5eddebad98a262ca886b
Bkzazzau1 <ibrahim.yahaya@miva.edu.ng>
2026-06-13 09:50:34 +0100

    Route students to upgraded live classroom room

--- lib/modules/live_sessions/view/live_session_room_view.dart
+++ lib/modules/live_sessions/view/live_session_room_view.dart
@@ -1,12 +1,20 @@
 import 'package:flutter/material.dart';
+import 'package:get/get.dart';
 
+import '../../../data/models/live_session_models.dart';
 import 'live_classroom_professional_shell.dart';
+import 'student_live_class_room_view.dart';
 
 class LiveSessionRoomView extends StatelessWidget {
   const LiveSessionRoomView({super.key});
 
   @override
   Widget build(BuildContext context) {
+    final args = (Get.arguments ?? {}) as Map;
+    final role = args['role']?.toString() ?? LiveSessionRole.student;
+    if (role == LiveSessionRole.student) {
+      return const StudentLiveClassRoomView();
+    }
     return const LiveClassroomProfessionalShell();
   }
 }
