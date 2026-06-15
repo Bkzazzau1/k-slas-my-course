d16a6cdfa74667b5eb2469d6a268e9e9e6addc8e
Ibrahim Bashir Yahaya <bkzazzau@gmail.com>
2026-06-15 19:28:24 +0100

    fixi4556ge4t

--- lib/main.dart
+++ lib/main.dart
@@ -3,10 +3,12 @@ import 'package:get_storage/get_storage.dart';
 
 import 'app/app.dart';
 import 'data/services/distance_learning_migration_service.dart';
+import 'features/identity_trust/services/identity_trust_demo_bootstrap.dart';
 
 Future<void> main() async {
   WidgetsFlutterBinding.ensureInitialized();
   await GetStorage.init();
   await DistanceLearningMigrationService.run();
+  IdentityTrustDemoBootstrap.register();
   runApp(const StudentAIApp());
 }
