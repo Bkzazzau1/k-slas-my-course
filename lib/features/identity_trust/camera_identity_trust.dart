028b1ab7a27dfebce39fe5097183e02b6de719ce
Bkzazzau1 <ibrahim.yahaya@miva.edu.ng>
2026-06-15 19:09:10 +0100

    Add camera identity trust exports

---
+++ lib/features/identity_trust/camera_identity_trust.dart
@@ -0,0 +1,7 @@
+export 'services/face_embedding_connector.dart';
+export 'services/face_image_decoder.dart';
+export 'services/face_image_preprocessor.dart';
+export 'services/jpeg_face_image_decoder.dart';
+export 'services/model_live_face_embedding_source.dart';
+export 'services/preprocessed_camera_face_embedding_input_provider.dart';
+export 'services/rgb_face_resizer.dart';
